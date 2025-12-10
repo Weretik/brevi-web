d8ff3a7dc131c2f5355d7efe8c7f68ffdb524433
Vitaliy <VitaliyThupin@gmail.com>
2025-12-10 07:53:11 +0200

    Update server routes to render 'admin' paths in client mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,9 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'admin/**',
+    renderMode: RenderMode.Client
+  },{
     path: '**',
     renderMode: RenderMode.Prerender
   }
