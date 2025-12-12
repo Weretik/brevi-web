28a4594ded72d82d6a7ed14f8e6a70b1dcf6ad36
Vitaliy <VitaliyThupin@gmail.com>
2025-12-12 08:35:26 +0200

    feat(storefront): add primeng button to home component
    - Imported and integrated PrimeNG Button module in the Home component.
    - Prepared for future UI enhancements involving interactive buttons.

--- src/app/storefront/features/home/home.ts
+++ src/app/storefront/features/home/home.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+
+import {Button} from 'primeng/button';
 import {Reference} from '../../../admin/features/reference';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    Button
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   public ref= new Reference();
+
 }
