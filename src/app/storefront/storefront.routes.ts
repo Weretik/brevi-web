e6be70ae5629da95c57a106a53a14aef2d9ace79
Vitaliy <VitaliyThupin@gmail.com>
2025-12-20 03:13:19 +0200

    feat(storefront): update routes and remove PrimeFlex import
    - Added routes for catalog and contacts pages under StorefrontLayout.
    - Removed unused PrimeFlex CSS import from styles for cleanup.

--- src/app/storefront/storefront.routes.ts
+++ src/app/storefront/storefront.routes.ts
@@ -1,11 +1,17 @@
 ﻿import { Routes } from '@angular/router';
 import { Home } from './pages/home';
 import { StorefrontLayout } from '@storefront/layout/storefront-layout/storefront-layout';
+import { Catalog } from '@storefront/pages/catalog';
+import { Contacts } from '@storefront/pages/contacts';
 
 export const storefrontRoutes: Routes = [
   {
     path: '',
     component: StorefrontLayout,
-    children: [{ path: '', component: Home }],
+    children: [
+      { path: '', component: Home },
+      { path: 'catalog', component: Catalog },
+      { path: 'contacts', component: Contacts },
+    ],
   },
 ];
