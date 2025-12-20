8cc1d1e86e6721547895a40f2709f3eff12c3ec6
Vitaliy <VitaliyThupin@gmail.com>
2025-12-20 20:56:45 +0200

    feat(storefront): integrate Tailwind CSS with initial configuration
    - Added Tailwind CSS setup and PostCSS configuration for utility-first styling.
    - Imported Tailwind CSS and PrimeIcons into styles for enhanced design flexibility.
    - Enhanced `Home` component with example Tailwind classes and reusable components for improved UI structure.

--- src/app/storefront/pages/home/home.ts
+++ src/app/storefront/pages/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Button } from 'primeng/button';
+import { WithBreadcrumbMetaAndActions } from '@storefront/shared/with-breadcrumb-meta-and-actions/with-breadcrumb-meta-and-actions';
+import { WithAvatarMetaAndActions } from '@storefront/shared/with-avatar-meta-and-actions/with-avatar-meta-and-actions';
 
 @Component({
   selector: 'app-home',
-  imports: [Button],
+  imports: [Button, WithBreadcrumbMetaAndActions, WithAvatarMetaAndActions],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
