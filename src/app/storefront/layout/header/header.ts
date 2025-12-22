239da6375976899b6ed6529a59945078c65d71a4
Vitaliy <VitaliyThupin@gmail.com>
2025-12-22 02:39:42 +0200

    feat(storefront): update header layout with new design and branding
    - Implemented a redesigned header layout with updated navigation links.
    - Added branding elements, including logo, search bar, and user avatar.
    - Improved accessibility, styling, and responsiveness for a better UX.

--- src/app/storefront/layout/header/header.ts
+++ src/app/storefront/layout/header/header.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IconField } from 'primeng/iconfield';
+import { InputIcon } from 'primeng/inputicon';
+import { NgOptimizedImage } from '@angular/common';
+import { InputText } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, IconField, InputIcon, InputText, NgOptimizedImage, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
