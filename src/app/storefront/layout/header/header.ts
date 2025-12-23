f8914b090549ab63246800735a2fe780d44b403e
Vitaliy <VitaliyThupin@gmail.com>
2025-12-23 23:28:57 +0200

    feat(storefront): enhance header with improved design and theme toggle
    - Updated header layout for better usability and branding alignment.
    - Added theme toggle functionality to switch between light and dark modes.
    - Improved responsiveness and accessibility for a seamless user experience.

--- src/app/storefront/layout/header/header.ts
+++ src/app/storefront/layout/header/header.ts
@@ -1,20 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { StyleClassModule } from 'primeng/styleclass';
 import { IconField } from 'primeng/iconfield';
 import { InputIcon } from 'primeng/inputicon';
-import { NgOptimizedImage } from '@angular/common';
 import { InputText } from 'primeng/inputtext';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { FormsModule } from '@angular/forms';
+import { ThemeService } from '@core/services/theme.service';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, IconField, InputIcon, InputText, NgOptimizedImage, RouterLinkActive],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    StyleClassModule,
+    IconField,
+    InputIcon,
+    NgOptimizedImage,
+    InputText,
+    ToggleButtonModule,
+    FormsModule,
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-  items = [
-    { label: 'Головна', routerLink: '/' },
-    { label: 'Каталог', routerLink: '/catalog' },
-    { label: 'Контакти', routerLink: '/contacts' },
-  ];
+export class Header implements OnInit {
+  private readonly themeService = inject(ThemeService);
+  public checked = false;
+
+  private ngOnInit(): void {
+    this.checked = this.themeService.getTheme();
+    this.themeService.setTheme(this.checked);
+  }
+
+  public onThemeToggle(value: boolean): void {
+    this.checked = value;
+    this.themeService.setTheme(value);
+  }
+
+  public toggleTheme(): void {
+    this.checked = !this.checked;
+    this.onThemeToggle(this.checked);
+  }
 }
