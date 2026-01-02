aeabc66ed74039c59bb235bec2e6abead390a3d6
Vitaliy <VitaliyThupin@gmail.com>
2026-01-02 21:14:07 +0200

    feat(storefront): add company-highlights section to home page
    - Introduced "Company Highlights" section to showcase key offerings and certifications.
    - Developed a responsive design using TailwindCSS for improved visual appeal.
    - Enhanced modular structure by creating a standalone Angular component.
    - Updated home page to include the new section for a richer user experience.

--- libs/storefront/feature/home/src/lib/page/home-page.ts
+++ libs/storefront/feature/home/src/lib/page/home-page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { AboutPreview } from '../sections/about-preview/about-preview';
+import { CompanyHighlights } from '../sections/company-highlights/company-highlights';
 import { MainHero } from '../sections/main-hero/main-hero';
 
 @Component({
   selector: 'lib-home-page',
-  imports: [MainHero, AboutPreview],
+  imports: [MainHero, AboutPreview, CompanyHighlights],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
---
+++ libs/storefront/feature/home/src/lib/sections/company-highlights/company-highlights.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-company-highlights',
+  imports: [],
+  templateUrl: './company-highlights.html',
+  styleUrl: './company-highlights.scss',
+})
+export class CompanyHighlights {}
