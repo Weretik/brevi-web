329158a674043cfcc479d8f6aa53b4208b7df5f7
Vitaliy <VitaliyThupin@gmail.com>
2026-01-02 18:03:06 +0200

    feat(storefront): add main-hero section to home page
    - Introduced a new "Main Hero" section to enhance the home page design.
    - Utilized a standalone Angular component for modularity and reusability.
    - Integrated responsive layout and styling using PrimeNG and TailwindCSS.

---
+++ libs/storefront/feature/home/src/lib/sections/main-hero/main-hero.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonDirective, ButtonLabel } from 'primeng/button';
+
+@Component({
+  selector: 'lib-main-hero',
+  imports: [ButtonLabel, ButtonDirective],
+  templateUrl: './main-hero.html',
+  styleUrl: './main-hero.scss',
+})
+export class MainHero {}
